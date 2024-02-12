dd8aa56fd66ad3f7eec9cc46378f67aa184a0443
Dududodedo11 <ec0061769@gmail.com>
2024-02-12 14:25:44 -0300

    Início do Header do App

---
+++ resources/views/app/layouts/_partials/header.blade.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="">
+        <img src="{{asset('images/logo_completa.png')}}" alt="Logo da Gestão Extrema">
+    </div>
+
+    <nav>
+        <ul>
+            <li>
+
+            </li>
+        </ul>
+    </nav>
+
+    <div class="">
+        
+    </div>
+</div>
--- resources/views/app/layouts/padrao.blade.php
+++ resources/views/app/layouts/padrao.blade.php
@@ -7,7 +7,9 @@
     <link rel="shortcut icon" href="{{asset('images/logo_qd.png')}}" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app/_partials/header.css')}}">
     @stack('links')
     <title>@yield('titulo') | Gestão Extrema</title>
 </head>
